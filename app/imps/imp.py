a5351dbe4a9f45f1a17ff9aa0ed158ec043b2127
alok620 <alok.shah620@gmail.com>
2022-07-13 11:48:11 -0700

    add all files

---
+++ app/imps/imp.py
@@ -0,0 +1,3 @@
+def getYay():
+    return "yay!"
+
---
+++ app/main.py
@@ -0,0 +1,35 @@
+from typing import Union
+
+from fastapi import FastAPI, status
+from pydantic import BaseModel
+
+from .imps import imp
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    yay = imp.getYay()
+    return {"Hello": yay}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+
+@app.get("/health/", status_code=200)
+async def health_check():
+    return {"Everything": "OK!"}
+
