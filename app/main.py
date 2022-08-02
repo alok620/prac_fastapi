2fdbec0a971da629eab2c4c095dc51af76aeb818
alok620 <alok.shah620@gmail.com>
2022-08-02 13:00:10 -0700

    predictions through frontend

--- app/main.py
+++ app/main.py
@@ -5,6 +5,11 @@ from pydantic import BaseModel
 
 from transformers import pipeline, set_seed
 
+from fastapi import FastAPI, status, Request, Form
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
+
 from .imps import imp
 
 class Item(BaseModel):
@@ -18,29 +23,45 @@ class Prediction(BaseModel):
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory='app/templates')
 
-@app.get("/")
-def read_root():
-    yay = imp.getYay()
-    return {"Hello": yay}
+app.mount('/static/', StaticFiles(directory='app/static'), name='static')
 
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse('home.html',
+            {'request': request})
+
+"""
 @app.post("/prediction")
 async def getPred(pred: Prediction):
     generator = pipeline('text-generation', model='gpt2')
     set_seed(103)
     res = generator(pred.instances[0]["instance_key_1"], max_length=30, num_return_sequences=5)
     return {"predictions": res}
+"""
+@app.post("/submit")
+async def submit(request: Request, taskname: str=Form(...)):
+    return f'YAY {taskname}'
 
-@app.get("/items/{item_id}")
+@app.post("/prediction")
+async def getPred(request: Request, prompt: str = Form(...)):
+    generator = pipeline('text-generation', model='gpt2')
+    set_seed(103)
+    res = generator(prompt, max_length=30, num_return_sequences=1)
+    return res[0]["generated_text"]
+
+
+@app.get("/items{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 
 @app.post("/items/")
-async def create_item(item: Item):
+def create_item(item: Item):
     return item
 
 @app.get("/health", status_code=200)
-async def health_check():
+def health_check():
     return {"Everything": "OK!"}
 
